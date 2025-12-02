5ad76528ff9c410925c6b816933bfb0c337d4654
Ben Devos <bendevos07@gmail.com>
2025-12-02 23:14:09 +0100

    fix: deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/advent-of-code-2025/',
   plugins: [
     tailwindcss(),
   ],
